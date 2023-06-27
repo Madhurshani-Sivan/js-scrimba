37c8f55f1398afc39ba91dba9e4d08f7ee2a2ed6
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-06-27 13:08:36 +0530

    blackjack

---
+++ blackjack/index.js
@@ -0,0 +1,72 @@
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+let sum = 0
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let cards = []
+let player = {
+  name: "Per",
+  chips: 200
+}
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function renderGame() {
+  cardsEl.textContent = "Cards: " 
+
+  for(let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
+
+  sumEl.textContent = "Sum: " + sum
+
+  if(sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if(sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  
+  messageEl.textContent = message
+}
+
+function newCard() {
+  if(isAlive && !hasBlackJack) {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
+  }
+}
+
+function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  renderGame()
+}
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1
+
+  if(randomNumber > 10) {
+    return 10
+  } else if(randomNumber === 1) {
+    return 11
+  } else {
+    return randomNumber
+  }
+
+}
+
+
+
